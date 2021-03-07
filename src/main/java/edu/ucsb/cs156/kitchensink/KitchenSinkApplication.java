eee04445e84239b940cd32494d1dfa23a170e5e5
Bryan Terce <bryan.terce@gmail.com>
2021-03-06 17:52:50 -0800

    Initial commit

---
+++ src/main/java/edu/ucsb/cs156/kitchensink/KitchenSinkApplication.java
@@ -0,0 +1,13 @@
+package edu.ucsb.cs156.kitchensink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchenSinkApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KitchenSinkApplication.class, args);
+  }
+
+}
---
+++ src/test/java/edu/ucsb/cs156/kitchensink/KitchenSinkApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ucsb.cs156.kitchensink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchenSinkApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
