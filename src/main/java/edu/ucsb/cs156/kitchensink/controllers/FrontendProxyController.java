e2655446e33dd68a37306fd77e1e18909520d89b
Bryan Terce <bryan.terce@gmail.com>
2021-03-16 16:58:20 -0700

    Improve error message when frontend isn't running in dev

--- src/main/java/edu/ucsb/cs156/kitchensink/controllers/FrontendProxyController.java
+++ src/main/java/edu/ucsb/cs156/kitchensink/controllers/FrontendProxyController.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.ResourceAccessException;
+
+import java.net.ConnectException;
 
 @Profile("development")
 @RestController
@@ -12,6 +15,13 @@ public class FrontendProxyController {
   @GetMapping({"/", "/{path:^(?!api|oauth2).*}/**"})
   public ResponseEntity<String> proxy(ProxyExchange<String> proxy) {
     String path = proxy.path("/");
-    return proxy.uri("http://localhost:3000/" + path).get();
+    try {
+      return proxy.uri("http://localhost:3000/" + path).get();
+    } catch (ResourceAccessException e) {
+      if (e.getCause() instanceof ConnectException) {
+        return ResponseEntity.ok("Failed to connect to the frontend server! You may have forgotten to run npm start in a separate ./dev_environment window (or it hasn't loaded yet).");
+      }
+      throw e;
+    }
   }
 }
