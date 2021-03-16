9f5e9dad5411f49cbc7247b331616e5c236d3609
Bryan Terce <bryan.terce@gmail.com>
2021-03-16 13:12:03 -0700

    Add email to users entity

---
+++ src/main/java/edu/ucsb/cs156/kitchensink/entities/User.java
@@ -0,0 +1,26 @@
+package edu.ucsb.cs156.kitchensink.entities;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Entity(name = "users")
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+  private String email;
+
+  public boolean isMember() {
+    return email.endsWith("@ucsb.edu");
+  }
+}
