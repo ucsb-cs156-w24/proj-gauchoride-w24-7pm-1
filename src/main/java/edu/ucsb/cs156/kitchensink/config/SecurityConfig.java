696a78acec128e2c5d5503706379fc39bafd0f02
Bryan Terce <bryan.terce@gmail.com>
2021-03-16 13:11:12 -0700

    Configure Spring method security

--- src/main/java/edu/ucsb/cs156/kitchensink/config/SecurityConfig.java
+++ src/main/java/edu/ucsb/cs156/kitchensink/config/SecurityConfig.java
@@ -1,14 +1,30 @@
 package edu.ucsb.cs156.kitchensink.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
 
 @Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests()
-        .anyRequest().permitAll();
+    http.authorizeRequests(authorize -> authorize
+          .anyRequest().permitAll()
+        )
+        .exceptionHandling(handlingConfigurer -> handlingConfigurer
+          .authenticationEntryPoint(new Http403ForbiddenEntryPoint())
+        )
+        .oauth2Login(oauth2 -> {});
+  }
+
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+    web.ignoring().antMatchers("/h2-console/**");
   }
 }
