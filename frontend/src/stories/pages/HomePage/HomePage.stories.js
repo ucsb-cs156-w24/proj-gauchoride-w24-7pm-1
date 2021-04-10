f9af2763852aef13376eacf07069a5d182c3dd3c
Phillip Conrad <pconrad@cs.ucsb.edu>
2021-04-10 15:53:38 -0700

    pc/bt - make username link to profile page
    And populate user model with all fields available in the OAuth2User object
    Plus, practice with Flyway database migrations

--- frontend/src/stories/pages/HomePage/HomePage.stories.js
+++ frontend/src/stories/pages/HomePage/HomePage.stories.js
@@ -12,20 +12,8 @@ export default {
     component: HomePage
 };
 
-
 const HomePageInContext = ContextTemplate(HomePage);
 const Template = () => <HomePageInContext />;
-
-// const Template = (args) =>  {
-//     const queryClient = new QueryClient();
-//     return (
-//         <QueryClientProvider client={queryClient}>
-//             <MemoryRouter>
-//                 <HomePage {...args} />
-//             </MemoryRouter>
-//         </QueryClientProvider>
-//     )
-// };
 
 export const Default = Template.bind({});
 
---
+++ src/main/resources/db/migration/V3__Add_fields_to_users.sql
@@ -0,0 +1,9 @@
+ ALTER TABLE users 
+ ADD (picture_url VARCHAR,
+      google_sub VARCHAR,
+      full_name VARCHAR,
+      given_name VARCHAR,
+      family_name VARCHAR,
+      email_verified BOOLEAN,
+      locale VARCHAR,
+      hosted_domain VARCHAR);
