14e34f27cad397fca4193be637c68f59ceca68cc
Phillip Conrad <pconrad@cs.ucsb.edu>
2021-04-25 15:57:02 -0700

    add plugin so that building storybook fails on most warnings
    The purpose is so that a bad storybook build will result
    in CI/CD not passing, so that we get an early warning
    sign of trouble with the storybook before merging in
    code that would break it.

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,3 +1,5 @@
+const WebpackPluginFailBuildOnWarning = require("./webpack-plugin-fail-build-on-warning");
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -7,5 +9,9 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app"
-  ]
+  ],
+  webpackFinal: async (config) => {
+    config.plugins.push(new WebpackPluginFailBuildOnWarning());
+    return config;
+  }
 }
