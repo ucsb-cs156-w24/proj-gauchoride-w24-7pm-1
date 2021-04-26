1656b33441a1192d1e871efd4cf49e07994451d9
Phillip Conrad <pconrad@cs.ucsb.edu>
2021-04-25 22:39:01 -0700

    pc - working roles ROLE_USER, ROLE_ADMIN, ROLE_MEMBER

--- src/main/java/edu/ucsb/cs156/kitchensink/config/SecurityConfig.java
+++ src/main/java/edu/ucsb/cs156/kitchensink/config/SecurityConfig.java
@@ -1,19 +1,47 @@
 package edu.ucsb.cs156.kitchensink.config;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
 import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import edu.ucsb.cs156.kitchensink.entities.User;
+import edu.ucsb.cs156.kitchensink.repositories.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@Slf4j
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Value("${app.admin.emails}")
+  final private List<String> adminEmails = new ArrayList<String>();
+
+  @Autowired
+  UserRepository userRepository;
+
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests(authorize -> authorize
@@ -22,7 +50,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .exceptionHandling(handlingConfigurer -> handlingConfigurer
           .authenticationEntryPoint(new Http403ForbiddenEntryPoint())
         )
-        .oauth2Login(oauth2 -> {})
+        // .oauth2Login(oauth2 -> {})
+        .oauth2Login(oauth2 -> oauth2.userInfoEndpoint(userInfo -> userInfo.userAuthoritiesMapper(this.userAuthoritiesMapper())))
         .csrf(csrf -> csrf
             .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
         )
@@ -36,5 +65,40 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   public void configure(WebSecurity web) throws Exception {
     web.ignoring().antMatchers("/h2-console/**");
+  }
+
+  private GrantedAuthoritiesMapper userAuthoritiesMapper() {
+    return (authorities) -> {
+      Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
+
+      authorities.forEach(authority -> {
+        log.info("********** authority={}", authority);
+        mappedAuthorities.add(authority);
+        if (OAuth2UserAuthority.class.isInstance(authority)) {
+          OAuth2UserAuthority oauth2UserAuthority = (OAuth2UserAuthority) authority;
+
+          Map<String, Object> userAttributes = oauth2UserAuthority.getAttributes();
+          log.info("********** userAttributes={}", userAttributes);
+
+          String email = (String) userAttributes.get("email");
+          if (isAdmin(email)) {
+            mappedAuthorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+          }
+
+          if (email.endsWith("@ucsb.edu")) {
+            mappedAuthorities.add(new SimpleGrantedAuthority("ROLE_MEMBER"));
+          }
+        }
+
+      });
+      return mappedAuthorities;
+    };
+  }
+  public boolean isAdmin(String email) {
+    if (adminEmails.contains(email)) {
+      return true;
+    }
+    Optional<User> u = userRepository.findByEmail(email);
+    return u.isPresent() && u.get().isAdmin();
   }
 }
