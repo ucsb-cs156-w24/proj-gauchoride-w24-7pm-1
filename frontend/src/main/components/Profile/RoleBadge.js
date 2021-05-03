be5f63af1f4dedb772201b0b31cdd507c26fe879
Phillip Conrad <pconrad@cs.ucsb.edu>
2021-05-02 17:50:25 -0700

    pc - Profile/RoleBadge full mutation test coverage

--- frontend/src/main/components/Profile/RoleBadge.js
+++ frontend/src/main/components/Profile/RoleBadge.js
@@ -5,10 +5,11 @@ export default function RoleBadge({role, currentUser}) {
    
   const roles = currentUser.root.roles.map( (o) => o.authority );
 
+  const lcrole = role.replace("ROLE_","").toLowerCase();
   return (
      roles.includes(role) ?
-        <Badge className="bg-primary" data-testid={"role-badge"}>{role.replace("ROLE_","").toLowerCase()}</Badge> 
+        <Badge className="bg-primary" data-testid={`role-badge-${lcrole}`}>{lcrole}</Badge> 
         :
-        <span className="missing-role" data-testid={"missing-role"} data-missingrole={role}></span>
+        <span data-testid={`role-missing-${lcrole}`}></span>
   );
 }
