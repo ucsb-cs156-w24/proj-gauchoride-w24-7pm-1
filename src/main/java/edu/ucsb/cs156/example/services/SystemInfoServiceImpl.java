5e0231119ea37c69d92a78aa227d525447226b22
Phillip Conrad <pconrad@cs.ucsb.edu>
2022-01-28 15:21:56 -0800

    pc - improve test coverage for backend basic infrastructure

--- src/main/java/edu/ucsb/cs156/example/services/SystemInfoServiceImpl.java
+++ src/main/java/edu/ucsb/cs156/example/services/SystemInfoServiceImpl.java
@@ -4,11 +4,15 @@ package edu.ucsb.cs156.example.services;
 import edu.ucsb.cs156.example.models.SystemInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Service;
 
+// This class relies on property values
+// For hints on testing, see: https://www.baeldung.com/spring-boot-testing-configurationproperties
 
 @Slf4j
 @Service("systemInfo")
+@ConfigurationProperties
 public class SystemInfoServiceImpl extends SystemInfoService {
   
   @Value("${spring.h2.console.enabled:false}")
