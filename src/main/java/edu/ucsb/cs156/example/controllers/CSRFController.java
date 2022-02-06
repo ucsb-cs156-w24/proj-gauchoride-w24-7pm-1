7ab63a0a85cbca77c2931bb63a3a5a1b29d2cbbc
Phill Conrad <phtcon@ucsb.edu>
2022-02-06 15:55:19 -0800

    pc - add test coverage and add endpoint to swagger

--- src/main/java/edu/ucsb/cs156/example/controllers/CSRFController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/CSRFController.java
@@ -5,9 +5,14 @@ import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @Profile("development")
+@Api(description = "CSRF")
 @RestController
 public class CSRFController {
+  @ApiOperation(value = "Get a CSRF Token")
   @GetMapping("/csrf")
   public CsrfToken csrf(CsrfToken token) {
     return token;
