3eb2cc03e14850f033939c279be8ffc1bc75bfd0
Phillip Conrad <pconrad@cs.ucsb.edu>
2022-02-12 14:08:05 -0800

    pc - remove commented out code

--- frontend/src/main/pages/AdminUsersPage.js
+++ frontend/src/main/pages/AdminUsersPage.js
@@ -2,9 +2,7 @@ import React from "react";
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 import UsersTable from "main/components/Users/UsersTable"
 
-// import { useQuery } from 'react-query'
 import useBackend from "main/utils/useBackend";
-// import useUsers from "main/queryclients/useUsers";
 const AdminUsersPage = () => {
 
     const { data: users, error: _error, status: _status } =
