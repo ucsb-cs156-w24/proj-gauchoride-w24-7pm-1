e29fd3d45f5e71bde4301ee3a85184ab35f8a50d
Phillip Conrad <pconrad@cs.ucsb.edu>
2022-02-14 13:38:19 -0800

    pc - add student fixture and StudentTable component in storybook

---
+++ frontend/src/fixtures/studentFixtures.js
@@ -0,0 +1,18 @@
+const studentFixtures = {
+ twoStudents: [
+    {
+      "id": "61fc469f26810d7b2d455a9d",
+      "firstName": "Chris",
+      "lastName": "Gaucho",
+      "perm": 1234567
+    },
+    {
+      "id": "6201887b3f8bf66ea1a6bd30",
+      "firstName": "Seth",
+      "lastName": "VanBrocklin",
+      "perm": 6666666
+    }
+  ]
+}
+
+export {studentFixtures }; 
---
+++ frontend/src/main/components/Students/StudentsTable.js
@@ -0,0 +1,34 @@
+import React from "react";
+import OurTable from "main/components/OurTable";
+
+export default function StudentsTable({ students, currentUser }) {
+
+    const columns = [
+        {
+            Header: 'id',
+            accessor: 'id', // accessor is the "key" in the data
+        },
+        {
+            Header: 'First Name',
+            accessor: 'firstName',
+        },
+        {
+            Header: 'Last Name',
+            accessor: 'lastName',
+        },
+        {
+            Header: 'Perm',
+            accessor: 'perm',
+        }
+    ];
+
+    // Stryker disable next-line ArrayDeclaration : [columns] is a performance optimization
+    const memoizedColumns = React.useMemo(() => columns, [columns]);
+    const memoizedDates = React.useMemo(() => students, [students]);
+
+    return <OurTable
+        data={memoizedDates}
+        columns={memoizedColumns}
+        testid={"StudentsTable"}
+    />;
+};
