08077468e1b696dfd0e14227c63c9fb1e8166784
andrewhlu <alu@ucsb.edu>
2022-04-14 01:37:54 -0700

    AL - Remove tests for Students menu

--- frontend/src/tests/components/Nav/AppNavbar.test.js
+++ frontend/src/tests/components/Nav/AppNavbar.test.js
@@ -212,55 +212,6 @@ describe("AppNavbar tests", () => {
         await waitFor( () => expect(getByTestId(/appnavbar-ucsbdates-create/)).toBeInTheDocument() );
 
     });
-
-    test("renders the Students menu correctly for a user", async () => {
-
-        const currentUser = currentUserFixtures.userOnly;
-        const systemInfo = systemInfoFixtures.showingBoth;
-
-        const doLogin = jest.fn();
-
-        const {getByTestId  } = render(
-            <QueryClientProvider client={queryClient}>
-                <MemoryRouter>
-                    <AppNavbar currentUser={currentUser} systemInfo={systemInfo} doLogin={doLogin} />
-                </MemoryRouter>
-            </QueryClientProvider>
-        );
-
-        await waitFor(() => expect(getByTestId("appnavbar-students-dropdown")).toBeInTheDocument());
-        const dropdown = getByTestId("appnavbar-students-dropdown");
-        const aElement = dropdown.querySelector("a");
-        expect(aElement).toBeInTheDocument();
-        aElement?.click();
-        await waitFor( () => expect(getByTestId("appnavbar-students-list")).toBeInTheDocument() );
-
-    });
-
-    test("renders the Students menu correctly for an admin", async () => {
-
-        const currentUser = currentUserFixtures.adminUser;
-        const systemInfo = systemInfoFixtures.showingBoth;
-
-        const doLogin = jest.fn();
-
-        const {getByTestId  } = render(
-            <QueryClientProvider client={queryClient}>
-                <MemoryRouter>
-                    <AppNavbar currentUser={currentUser} systemInfo={systemInfo} doLogin={doLogin} />
-                </MemoryRouter>
-            </QueryClientProvider>
-        );
-
-        await waitFor(() => expect(getByTestId("appnavbar-students-dropdown")).toBeInTheDocument());
-        const dropdown = getByTestId("appnavbar-students-dropdown");
-        const aElement = dropdown.querySelector("a");
-        expect(aElement).toBeInTheDocument();
-        aElement?.click();
-        await waitFor( () => expect(getByTestId(/appnavbar-students-create/)).toBeInTheDocument() );
-
-    });
-
 });
 
 
