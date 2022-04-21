43e5ccaa1905d512f984dc6a5bc26a771b236c0a
Phillip Conrad <pconrad@cs.ucsb.edu>
2022-04-20 18:50:54 -0700

    pc - minor cleanup in entity class

--- src/main/java/edu/ucsb/cs156/example/entities/UCSBDiningCommons.java
+++ src/main/java/edu/ucsb/cs156/example/entities/UCSBDiningCommons.java
@@ -4,7 +4,6 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,7 +12,6 @@ import lombok.Builder;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Getter
 @Entity(name = "ucsbdiningcommons")
 public class UCSBDiningCommons {
   @Id
@@ -24,10 +22,4 @@ public class UCSBDiningCommons {
   private boolean hasDiningCam;
   private Double latitude;
   private Double longitude;
-
-  // public boolean getHasSackMeal() { return this.hasSackMeal; }
-  // public boolean getHasTakeOutMeal() { return this.hasTakeOutMeal; }
-  // public boolean getHasDiningCam() { return this.hasDiningCam; }
-
-
 }
