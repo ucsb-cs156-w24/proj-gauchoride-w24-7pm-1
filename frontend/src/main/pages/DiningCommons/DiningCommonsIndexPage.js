623410861c7c0272bf6074a21c9885800e2f9bc9
Phillip Conrad <pconrad@cs.ucsb.edu>
2022-05-05 14:31:19 -0700

    pc - add dining commons index page

---
+++ frontend/src/main/pages/DiningCommons/DiningCommonsIndexPage.js
@@ -0,0 +1,14 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+export default function DiningCommonsIndexPage() {
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Dining Commons</h1>
+        <p>
+          This is where the index page will go
+        </p>
+      </div>
+    </BasicLayout>
+  )
+}
