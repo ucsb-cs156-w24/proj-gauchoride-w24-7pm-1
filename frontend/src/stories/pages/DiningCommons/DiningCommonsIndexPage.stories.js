92d0ad772904480a5292e5462355c090c59031d0
Phillip Conrad <pconrad@cs.ucsb.edu>
2022-05-12 13:33:56 -0700

    pc - add DiningCommonsIndexPage to storybook

---
+++ frontend/src/stories/pages/DiningCommons/DiningCommonsIndexPage.stories.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+import DiningCommonsIndexPage from "main/pages/DiningCommons/DiningCommonsIndexPage";
+
+export default {
+    title: 'pages/DiningCommons/DiningCommonsIndexPage',
+    component: DiningCommonsIndexPage
+};
+
+const Template = () => <DiningCommonsIndexPage />;
+
+export const Default = Template.bind({});
+
+
+
+
