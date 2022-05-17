19070644ac4ae9c6256867325e0d6cfa1798bfa6
Phillip Conrad <pconrad@cs.ucsb.edu>
2022-05-17 13:45:59 -0700

    pc - add placeholder for Dining Commons Create Page and a route to it

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import TodosCreatePage from "main/pages/Todos/TodosCreatePage";
 import TodosEditPage from "main/pages/Todos/TodosEditPage";
 
 import DiningCommonsIndexPage from "main/pages/DiningCommons/DiningCommonsIndexPage";
+import DiningCommonsCreatePage from "main/pages/DiningCommons/DiningCommonsCreatePage";
 
 import UCSBDatesIndexPage from "main/pages/UCSBDates/UCSBDatesIndexPage";
 import UCSBDatesCreatePage from "main/pages/UCSBDates/UCSBDatesCreatePage";
@@ -44,6 +45,7 @@ function App() {
           hasRole(currentUser, "ROLE_USER") && (
             <>
               <Route exact path="/diningCommons/list" element={<DiningCommonsIndexPage />} />
+              <Route exact path="/diningCommons/create" element={<DiningCommonsCreatePage />} />
             </>
           )
         }
---
+++ frontend/src/main/pages/DiningCommons/DiningCommonsCreatePage.js
@@ -0,0 +1,14 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+export default function DiningCommonsCreatePage() {
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Dining Commons</h1>
+        <p>
+          This is where the create page will go
+        </p>
+      </div>
+    </BasicLayout>
+  )
+}
