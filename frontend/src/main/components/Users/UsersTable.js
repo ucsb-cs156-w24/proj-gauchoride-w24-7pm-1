bb4b43f284d17584fd9958a4cfdd767ea8829c57
Sebastian Anvari <sebastiananvari@Sebastians-MacBook-Pro-3.local>
2022-12-14 00:10:47 -0800

    button column frontend but not working (2)

--- frontend/src/main/components/Users/UsersTable.js
+++ frontend/src/main/components/Users/UsersTable.js
@@ -1,43 +1,71 @@
-import React from "react";
+
 import OurTable, { ButtonColumn } from "main/components/OurTable"
+import { useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
 
-const columns = [
-    {
-        Header: 'id',
-        accessor: 'id', // accessor is the "key" in the data
-    },
-    {
-        Header: 'First Name',
-        accessor: 'givenName',
-    },
-    {
-        Header: 'Last Name',
-        accessor: 'familyName',
-    },
-    {
-        Header: 'Email',
-        accessor: 'email',
-    },
-    {
-        Header: 'Admin',
-        id: 'admin',
-        accessor: (row, _rowIndex) => String(row.admin), // hack needed for boolean values to show up
-    },
-    {
-        Header: 'Driver',
-        id: 'driver',
-        accessor: (row, _rowIndex) => String(row.driver) // hack needed for boolean values to show up
+
+//sa: check for admin status for showbuttons I assume
+export default function UsersTable({ users, showButtons=true }) {
+
+    function cellToAxiosParamsToggleAdmin(cell) {
+        return {
+            url: "/api/users/admin/toggleAdmin/",
+            method: "POST",
+            params: {
+                id: cell.row.values.id
+            }
+        }
     }
-];
+     function onToggleAdminSuccess(message) {
+        console.log(message);
+        toast(message);
+    }
+    // Stryker disable all : hard to test for query caching
+    const toggleAdminMutation = useBackendMutation(
+        cellToAxiosParamsToggleAdmin,
+        { onSuccess: onToggleAdminSuccess },
+        ["/api/admin/users"]
+    );
+    // Stryker enable all 
 
-const buttonColumns = [
-    ...columns,
-        ButtonColumn("Toggle Admin", "danger", postCallback, "UsersTable")
-]
+    // Stryker disable next-line all : TODO try to make a good test for this
+    const toggleAdminCallback = async (cell) => { toggleAdminMutation.mutate(cell); }
 
-export default function UsersTable({ users }) {
-    return <OurTable
-        data={users}
-        columns={columns}
-        testid={"UsersTable"} />;
+
+    const columns = [
+        {
+            Header: 'id',
+            accessor: 'id', // accessor is the "key" in the data
+        },
+        {
+            Header: 'First Name',
+            accessor: 'givenName',
+        },
+        {
+            Header: 'Last Name',
+            accessor: 'familyName',
+        },
+        {
+            Header: 'Email',
+            accessor: 'email',
+        },
+        {
+            Header: 'Admin',
+            id: 'admin',
+            accessor: (row, _rowIndex) => String(row.admin) // hack needed for boolean values to show up
+        },
+        {
+            Header: 'Driver',
+            id: 'driver',
+            accessor: (row, _rowIndex) => String(row.driver) // hack needed for boolean values to show up
+        }
+    ];
+
+    const buttonColumns = [
+        ...columns,
+        ButtonColumn("Toggle Admin", "primary", toggleAdminCallback, "UsersTable"),
+    ]
+
+    const columnsToDisplay = showButtons ? buttonColumns : columns;
+
 };
