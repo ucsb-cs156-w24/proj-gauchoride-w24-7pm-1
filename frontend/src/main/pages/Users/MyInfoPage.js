72aff2f0239f563963cead72cf51b287a671beb3
Phillip Conrad <pconrad@cs.ucsb.edu>
2022-12-16 10:21:29 -0800

    pc - WIP on MyInfoPage

---
+++ frontend/src/main/pages/Users/MyInfoPage.js
@@ -0,0 +1,14 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+export default function MyInfoPage() {
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>My Information</h1>
+        <p>
+          This page is a placeholder; it will eventually show information about the current user.
+        </p>
+      </div>
+    </BasicLayout>
+  )
+}
---
+++ frontend/src/stories/pages/Users/MyInfoPage.stories.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+import MyInfoPage from "main/pages/Users/MyInfoPage";
+
+export default {
+    title: 'pages/Users/MyInfoPage',
+    component: MyInfoPage
+};
+
+const Template = () => <MyInfoPage />;
+
+export const Default = Template.bind({});
+
+
+
+
