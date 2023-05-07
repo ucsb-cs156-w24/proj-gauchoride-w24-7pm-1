cc861dc2d8d4748c2c39ecad700df4da6842ba47
Phill Conrad <phtcon@ucsb.edu>
2023-05-07 11:46:59 -0700

    pc - add some more javadoc comments

--- src/main/java/edu/ucsb/cs156/gauchoride/controllers/ApiController.java
+++ src/main/java/edu/ucsb/cs156/gauchoride/controllers/ApiController.java
@@ -26,14 +26,29 @@ public abstract class ApiController {
   @Autowired
   private CurrentUserService currentUserService;
 
+  /**
+   * Get the current user
+   * @return the current user
+   */
+
   protected CurrentUser getCurrentUser() {
     return currentUserService.getCurrentUser();
   }
+
+
+  /**
+   * This creates a plain old java object that can be returned as a JSON response
+   * @return a Map object with a single key/value pair: "message" => message
+   */
 
   protected Object genericMessage(String message) {
     return Map.of("message", message);
   }
 
+   /**
+   * This catches any EntityNotFoundExceptions and returns a 404 (NOT_FOUND) response
+   * @return a Map object that can be returned as a JSON response
+   */
   @ExceptionHandler({ EntityNotFoundException.class })
   @ResponseStatus(HttpStatus.NOT_FOUND)
   public Object handleGenericException(Throwable e) {
