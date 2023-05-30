443b888df46365ffa7f8a324be5de8afd71f2274
Cheryl Stanley <cherylstanley@ucsb.edu>
2023-05-30 00:44:35 -0700

    Added a 404 page notifying users when they have reached a url that doesn't exist and incorporating button redirecting to HomePage

---
+++ frontend/src/main/pages/PageNotFound.js
@@ -0,0 +1,33 @@
+import React from "react";
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import {useNavigate } from "react-router-dom";
+
+// import ReactJson from "react-json-view";
+
+
+const PageNotFound = () => {
+    const navigate = useNavigate();
+  
+  const navigateHome = () => {
+    navigate('/');
+  };
+    return(
+      <BasicLayout>
+        <head>
+      <script type="text/javascript" src="./main/pages/HomePage.js"></script>
+      </head>
+        <div className="pt-2">
+          <h1>404 - Request URL is Not Found</h1>
+          <p>
+           You do not have authorization to access this page or this page does not exist
+          </p>
+          {/* <button onclick ={<HomePage></HomePage>}>Back to Home Page</button> */}
+  
+          <button class="btn btn-primary" onClick={navigateHome}>Go Back to Home Page</button>
+  
+        </div>
+      </BasicLayout>
+    );
+  }
+
+  export default PageNotFound;
