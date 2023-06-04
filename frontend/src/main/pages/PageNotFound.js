9ff2e1ec7eb80b0419ba89685321886214a8b9a8
Cheryl Stanley <cherylstanley@ucsb.edu>
2023-06-04 16:15:12 -0700

    Removed unnecessary comment

--- frontend/src/main/pages/PageNotFound.js
+++ frontend/src/main/pages/PageNotFound.js
@@ -2,7 +2,6 @@ import React from "react";
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 import {useNavigate } from "react-router-dom";
 
-// import ReactJson from "react-json-view";
 
 
 const PageNotFound = () => {
@@ -20,9 +19,7 @@ const PageNotFound = () => {
           <h1>404 - Request URL is Not Found</h1>
           <p>
            You do not have authorization to access this page or this page does not exist
-          </p>
-          {/* <button onclick ={<HomePage></HomePage>}>Back to Home Page</button> */}
-  
+          </p>  
           <button class="btn btn-primary" onClick={navigateHome}>Go Back to Home Page</button>
   
         </div>
