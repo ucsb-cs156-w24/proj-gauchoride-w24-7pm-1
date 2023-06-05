dcc9f96b45e4890770eed740926f3222c152fb1d
Cheryl Stanley <cherylstanley@ucsb.edu>
2023-06-04 18:14:24 -0700

    Customized HomePage including usage of official UCSB brand approved CSS style / colors and a description of the purpose and goals of the GauchoRide application

--- frontend/src/main/pages/HomePage.js
+++ frontend/src/main/pages/HomePage.js
@@ -3,11 +3,18 @@ import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 export default function HomePage() {
   return (
     <BasicLayout>
+      <head></head>
       <div className="pt-2">
-        <h1>Hello, world!</h1>
+      <h1 style = {{ padding: "20px"}}> Welcome to       <b style = {{backgroundColor:"#003660", padding: "0px 10px"}}> <font color = "#FFFFFF"  >UCSB</font></b> GauchoRide!</h1>
+      <div style={{margin:"0px 20px", backgroundColor:"#d6d2d2", padding: "20px"}}>
+      <h3>About this application</h3>
         <p>
-          This is a webapp containing a bunch of different Spring Boot/React examples.
+        This app is being built by the students of CMPSC 156 at UCSB to assist an effort to provide transportation for UCSB students with mobility issues to be better able to get to and from class.
         </p>
+        <p><i>The first iteration of this app was created with a simple goal in mind: to make navigating to and from school as quick and easy as possible by connecting students in need with available drivers.
+          As the app progresses, we hope to add new features and make improvements for a better overall experience for our users.
+          </i>  </p>
+      </div>
       </div>
     </BasicLayout>
   )
