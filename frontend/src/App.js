d5e570ddd9f1016198035a16066c1fe1f06bc074
Ian Wen <h_wen@ucsb.edu>
2023-06-06 15:49:15 -0700

    iw - removing route to shifttable page until backend is working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "main/pages/HomePage";
 import ProfilePage from "main/pages/ProfilePage";
 import AdminUsersPage from "main/pages/AdminUsersPage";
-import ShiftPage from "main/pages/ShiftPage";
+
 
 
 
@@ -23,15 +23,6 @@ function App() {
         <Route exact path="/profile" element={<ProfilePage />} />
         {
           hasRole(currentUser, "ROLE_ADMIN") && <Route exact path="/admin/users" element={<AdminUsersPage />} />
-        }
-        {
-          hasRole(currentUser, "ROLE_ADMIN") && <Route exact path="/shift/list" element={<ShiftPage />} />
-        }
-        {
-          hasRole(currentUser, "ROLE_DRIVER") && <Route exact path="/shift/list" element={<ShiftPage />} />
-        }
-        {
-          hasRole(currentUser, "ROLE_RIDER") && <Route exact path="/shift/list" element={<ShiftPage />} />
         }
         {
           hasRole(currentUser, "ROLE_USER")
