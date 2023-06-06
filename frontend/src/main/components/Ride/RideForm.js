f4970c992068b40c5229850de0ed1a2b921b92ec
SophiaWeiler <sophia.weiler@outlook.com>
2023-06-05 20:24:42 -0700

    sw - changes to RideForm

--- frontend/src/main/components/Ride/RideForm.js
+++ frontend/src/main/components/Ride/RideForm.js
@@ -79,7 +79,6 @@ function RideForm({ initialContents, submitAction, buttonLabel = "Create" }) {
                           }
                     })}
                     placeholder="Enter time in the format HH:MM AM/PM (e.g. 3:30PM)"
-                    value={initialContents?.startTime}
                 />
                 <Form.Control.Feedback type="invalid">
                     {errors.start?.message}
@@ -100,8 +99,7 @@ function RideForm({ initialContents, submitAction, buttonLabel = "Create" }) {
                             message: "Please enter time in the format HH:MM AM/PM (e.g., 3:30PM)."
                           }
                     })}
-                    placeholder="Enter time in the format HH:MM AM/PM (e.g. 3:30PM)"     
-                    value={initialContents?.endTime}   
+                    placeholder="Enter time in the format HH:MM AM/PM (e.g. 3:30PM)"      
                 />
                 <Form.Control.Feedback type="invalid">
                     {errors.end?.message}
@@ -119,7 +117,6 @@ function RideForm({ initialContents, submitAction, buttonLabel = "Create" }) {
                         required: "Pick Up Location is required."
                     })}
                     placeholder="e.g. Anacapa Residence Hall"  
-                    value={initialContents?.pickupLocation} 
                 />
                 <Form.Control.Feedback type="invalid">
                     {errors.pickup?.message}
@@ -137,7 +134,6 @@ function RideForm({ initialContents, submitAction, buttonLabel = "Create" }) {
                         required: "Drop Off Location is required."
                     })}
                     placeholder="e.g. Phelps"  
-                    value={initialContents?.dropoffLocation}
                 />
                 <Form.Control.Feedback type="invalid">
                     {errors.dropoff?.message}
@@ -155,7 +151,6 @@ function RideForm({ initialContents, submitAction, buttonLabel = "Create" }) {
                         required: "Room number is required."
                     })}
                     placeholder="e.g. 2225"  
-                    value={initialContents?.room} 
                 />
                 <Form.Control.Feedback type="invalid">
                     {errors.room?.message}
@@ -173,7 +168,6 @@ function RideForm({ initialContents, submitAction, buttonLabel = "Create" }) {
                         required: "Course number is required."
                     })}
                     placeholder="e.g. CMPSC 156"  
-                    value={initialContents?.course} 
                 />
                 <Form.Control.Feedback type="invalid">
                     {errors.course?.message}
