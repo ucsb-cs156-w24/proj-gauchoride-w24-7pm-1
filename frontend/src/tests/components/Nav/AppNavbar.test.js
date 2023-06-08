aa38f31c27335278ac3612a0b1f314fcd484672f
Phillip Conrad <pconrad@cs.ucsb.edu>
2023-06-08 09:48:35 -0700

    pc - test that create ride doesn't show up for ordinary user

--- frontend/src/tests/components/Nav/AppNavbar.test.js
+++ frontend/src/tests/components/Nav/AppNavbar.test.js
@@ -356,6 +356,24 @@ describe("AppNavbar tests", () => {
         expect(rideMenu).not.toBeInTheDocument();        
     });
 
+    test("Create ride should not be available for user that is neither Admin nor Rider", async () => {
+
+        const currentUser = currentUserFixtures.userOnly;
+        const doLogin = jest.fn();
+
+        const { getByText } = render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <AppNavbar currentUser={currentUser} doLogin={doLogin} />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+        
+        await waitFor(() => expect(getByText("Welcome, Phillip Conrad")).toBeInTheDocument());
+        const rideMenu = screen.queryByTestId("appnavbar-ride-create-dropdown");
+        expect(rideMenu).not.toBeInTheDocument();        
+    });
+
 });
 
 
