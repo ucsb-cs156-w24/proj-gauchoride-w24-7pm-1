502889f11393b3cde58b7e226c63c2524da2d41d
Shiyang/Bill <shiyangxin@ucsb.edu>
2023-08-17 14:44:15 -0700

    change pp.js back to original
    change pp.js back to original

--- frontend/src/main/pages/PrivacyPolicy.js
+++ frontend/src/main/pages/PrivacyPolicy.js
@@ -5,17 +5,17 @@ import React from "react";
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 import UsersTable from "main/components/Users/UsersTable"
 
-//import { useBackend } from "main/utils/useBackend";
+import { useBackend } from "main/utils/useBackend";
 const AdminUsersPage = () => {
 
-    //const { data: users, error: _error, status: _status } =
-      //  useBackend(
-        //    // Stryker disable next-line all : don't test internal caching of React Query
-          //  ["/api/admin/users"],
+    const { data: users, error: _error, status: _status } =
+        useBackend(
+            // Stryker disable next-line all : don't test internal caching of React Query
+            ["/api/admin/users"],
             // Stryker disable next-line StringLiteral,ObjectLiteral : since "GET" is default, "" is an equivalent mutation
-            //{ method: "GET", url: "/api/admin/users" },
-            //[]
-        //);
+            { method: "GET", url: "/api/admin/users" },
+            []
+        );
 
     return (
         <BasicLayout>
