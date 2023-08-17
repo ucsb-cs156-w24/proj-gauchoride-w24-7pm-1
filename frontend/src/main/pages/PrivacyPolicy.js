826396ffc83a66a781ee0da243ae5afa62916286
Shiyang/Bill <shiyangxin@ucsb.edu>
2023-08-17 14:53:49 -0700

    Change pp.js
    Change pp.js

--- frontend/src/main/pages/PrivacyPolicy.js
+++ frontend/src/main/pages/PrivacyPolicy.js
@@ -1,29 +1,14 @@
-
-
-
-import React from "react";
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
-import UsersTable from "main/components/Users/UsersTable"
 
-import { useBackend } from "main/utils/useBackend";
-const AdminUsersPage = () => {
-
-    const { data: users, error: _error, status: _status } =
-        useBackend(
-            // Stryker disable next-line all : don't test internal caching of React Query
-            ["/api/admin/users"],
-            // Stryker disable next-line StringLiteral,ObjectLiteral : since "GET" is default, "" is an equivalent mutation
-            { method: "GET", url: "/api/admin/users" },
-            []
-        );
-
-    return (
-        <BasicLayout>
-            <h2>Privacy Policy</h2> https://termly.io/products/privacy-policy-generator
-            <UsersTable users={users} />
-        </BasicLayout>
-    );
-};
-
-export default AdminUsersPage;
-//footer
+export default function PrivacyPolicy() {
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h2>PrivacyPolicy</h2>
+        <p>
+            https://github.com/ucsb-cs156/proj-gauchoride/blob/main/frontend/public/privacy.html
+        </p>
+      </div>
+    </BasicLayout>
+  )
+}
