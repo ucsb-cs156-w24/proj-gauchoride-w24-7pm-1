1c174d204c3efe7bf952860752294e1fe527e578
Charlie Jyu <cjyu@ucsb.edu>
2023-08-20 08:47:08 -0700

    finish createpage, story, and test; all tests and mutations pass

---
+++ frontend/src/stories/pages/Shift/ShiftCreatePage.stories.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
+import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+import { rest } from "msw";
+
+import ShiftCreatePage from "main/pages/Shift/ShiftCreatePage"
+
+export default {
+    title: 'pages/Shift/ShiftCreatePage',
+    component: ShiftCreatePage
+};
+
+const Template = () => <ShiftCreatePage storybook={true} />;
+
+export const Default = Template.bind({});
+Default.parameters = {
+    msw: [
+        rest.get('/api/currentUser', (_req, res, ctx) => {
+            return res(ctx.json(apiCurrentUserFixtures.userOnly));
+        }),
+        rest.get('/api/systemInfo', (_req, res, ctx) => {
+            return res(ctx.json(systemInfoFixtures.showingNeither));
+        }),
+        rest.post('/api/shift/post', (req, res, ctx) => {
+            window.alert("POST: " + JSON.stringify(req.url));
+            return res(ctx.status(200),ctx.json({}));
+        }),
+    ]
+}
+
+
+
