8376e9ac346a250bb59cfe2079d578aeb956312e
abcdefucsb <shiyangxin@ucsb.edu>
2023-08-22 15:06:12 -0700

    modified pp.js and added the html file

--- frontend/src/main/pages/PrivacyPolicy.js
+++ frontend/src/main/pages/PrivacyPolicy.js
@@ -1,12 +1,14 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 
+
 export default function PrivacyPolicy() {
   return (
     <BasicLayout>
       <div className="pt-2">
         <h2>Privacy Policy</h2> 
         <p>
-            PLACEHOLDER HTML FILE WILL BE INCLUDED
+            
+            <a href="privacy.html">Go to Privacy Policy</a>
         </p>
       </div>
     </BasicLayout>
