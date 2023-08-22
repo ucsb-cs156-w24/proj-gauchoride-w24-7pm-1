177c261f863cf60a70186b2944f5f781fef7a470
Andrew Thai <andrewthai@169-231-167-12.wireless.ucsb.edu>
2023-08-22 15:28:38 -0700

    wrote most of fixture and a change in entity

---
+++ frontend/src/fixtures/riderApplicationFixtures.js
@@ -0,0 +1,53 @@
+const riderApplicationFixtures = {
+    oneRiderApplication:
+    [
+      {
+       "id": 1,
+        "status": "deny",
+        "perm_number": "123",
+        "created_date": "2023- 06 -22 05:22:22",
+        "updated_date": "2023- 07 -22 05:22:22",
+        "cancelled_date": "2023- 07 -25 05:22:22",
+        "description": "I have broken leg",
+        "notes": "no notes"    
+      }
+    ],
+    threeRiderApplication:
+    [
+        {
+            "id": 2,
+            "status": "approved",
+            "perm_number": "456",
+            "created_date": "2023-07-22T05:22:22",
+            "updated_date": "2023- 08 -22 05:22:22",
+            "cancelled_date": "2023- 09 -25 05:22:22",
+            "description": "I have broken arm",
+            "notes": "no notes"
+        },
+
+        {
+            "id": 3,
+            "status": "deny",
+            "perm_number": "789",
+            "created_date": "2023- 07 -22 05:22:22",
+            "updated_date": "2023- 08 -22 05:22:22",
+            "cancelled_date": "2023- 09 -25 05:22:22",
+            "description": "I have broken foot",
+            "notes": "some notes" 
+        },
+
+        {
+            "id": 4,
+            "status": "approved",
+            "perm_number": "101112",
+            "created_date": "2023- 10 -22 05:22:22",
+            "updated_date": "2023- 11 -22 05:22:22",
+            "cancelled_date": "2023- 12 -25 05:22:22",
+            "description": "I have broken finger",
+            "notes": "long notes"
+        },
+        
+    ]
+};
+
+export { riderApplicationFixtures };
