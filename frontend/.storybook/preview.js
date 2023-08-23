b119e77e22355361a52013488d3f67807d947b06
Mohamed-Elfouly <elfouly@ucsb.edu>
2023-08-22 18:14:01 -0700

    Tests not done
    Tests not Done

--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -1,6 +1,8 @@
 import "bootstrap/dist/css/bootstrap.css";
 import 'react-toastify/dist/ReactToastify.css';
 import "../src/index.css";
+import { initialize, mswDecorator } from 'msw-storybook-addon';
+
 
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
@@ -8,6 +10,18 @@ import { MemoryRouter } from "react-router-dom";
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
+
+const currentUrl = window.location.href;
+const isLocalhost = currentUrl.startsWith("http://localhost:6006/");
+const mockServiceWorkerUrl = isLocalhost ? "mockServiceWorker.js" : "https://" + window.location.hostname + "/mockServiceWorker.js";
+
+initialize(
+  {
+    serviceWorker: {
+      url: mockServiceWorkerUrl
+    }
+  }
+);
 
 const queryClient = new QueryClient();
 
@@ -23,4 +37,8 @@ export const decorators = [
       </MemoryRouter>
     </QueryClientProvider>
   ),
+  mswDecorator, // provide the MSW decorator globally
 ];
+
+
+
