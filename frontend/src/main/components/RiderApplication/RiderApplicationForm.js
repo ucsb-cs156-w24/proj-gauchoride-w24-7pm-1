bae903092d5048b354cafc36e568c31f290237ef
jiangtianwang <jiangtianwang@ucsb.edu>
2023-08-23 23:04:00 -0700

    implemented Form component of riderApplication; 100% mutation/line coverage

---
+++ frontend/src/main/components/RiderApplication/RiderApplicationForm.js
@@ -0,0 +1,183 @@
+import React from 'react'
+import { Button, Form } from 'react-bootstrap';
+import { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom';
+
+function RiderApplicationForm({ initialContents, submitAction, buttonLabel = "Create" }) {
+    const navigate = useNavigate();
+    
+    // Stryker disable all
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+    } = useForm(
+        { defaultValues: initialContents }
+    );
+    // Stryker enable all
+   
+    const testIdPrefix = "RiderApplicationForm";
+
+
+    return (
+
+        <Form onSubmit={handleSubmit(submitAction)}>
+
+            {initialContents && (
+                <Form.Group className="mb-3" >
+                    <Form.Label htmlFor="id">Applicant Id</Form.Label>
+                    <Form.Control
+                        data-testid={testIdPrefix + "-id"}
+                        id="id"
+                        type="text"
+                        {...register("id")}
+                        defaultValue={initialContents?.userId}
+                        disabled
+                    />
+                </Form.Group>
+            )}
+
+            {initialContents && (
+                <Form.Group className="mb-3" >
+                    <Form.Label htmlFor="status">Status</Form.Label>
+                    <Form.Control
+                        data-testid={testIdPrefix + "-status"}
+                        id="status"
+                        type="text"
+                        {...register("status")}
+                        defaultValue={initialContents?.status}
+                        disabled
+                    />
+                </Form.Group>
+            )}
+
+            <Form.Group className="mb-3" >
+                <Form.Label htmlFor="email">Email</Form.Label>
+                <Form.Control
+                    data-testid={testIdPrefix + "-email"}
+                    id="email"
+                    type="text"
+                    {...register("email")}
+                    defaultValue={initialContents?.email}
+                    disabled
+                />
+            </Form.Group>
+
+            {initialContents && (
+                <Form.Group className="mb-3" >
+                    <Form.Label htmlFor="created_date">Date Applied</Form.Label>
+                    <Form.Control
+                        data-testid={testIdPrefix + "-created_date"}
+                        id="created_date"
+                        type="text"
+                        {...register("created_date")}
+                        defaultValue={initialContents?.created_date}
+                        disabled
+                    />
+                </Form.Group>
+            )}
+            
+            {initialContents && (
+                <Form.Group className="mb-3" >
+                    <Form.Label htmlFor="updated_date">Date Updated</Form.Label>
+                    <Form.Control
+                        data-testid={testIdPrefix + "-updated_date"}
+                        id="updated_date"
+                        type="text"
+                        {...register("updated_date")}
+                        defaultValue={initialContents?.updated_date}
+                    />
+                </Form.Group>
+            )}
+
+            {initialContents && (
+                <Form.Group className="mb-3" >
+                    <Form.Label htmlFor="cancelled_date">Date Cancelled</Form.Label>
+                    <Form.Control
+                        data-testid={testIdPrefix + "-cancelled_date"}
+                        id="cancelled_date"
+                        type="text"
+                        {...register("cancelled_date")}
+                        defaultValue={initialContents?.cancelled_date}
+                    />
+                </Form.Group>
+            )}
+
+            {initialContents && (
+                <Form.Group className="mb-3" >
+                    <Form.Label htmlFor="notes">Notes</Form.Label>
+                    <Form.Control
+                        data-testid={testIdPrefix + "-notes"}
+                        id="notes"
+                        type="text"
+                        {...register("notes")}
+                        defaultValue={initialContents?.notes}
+                        disabled
+                    />
+                </Form.Group>
+            )}
+
+            <Form.Group className="mb-3">
+                <Form.Label htmlFor="perm_number">Perm Number</Form.Label>
+                <Form.Control
+                    data-testid={testIdPrefix + "-perm_number"}
+                    id="perm_number"
+                    type="text"
+                    isInvalid={Boolean(errors.perm_number)}
+                    {...register("perm_number", {
+                        required: "Perm Number is required.",
+                        minLength: {
+                            value: 7,
+                            message: "Perm Number must be exactly 7 characters long."
+                        },
+                        maxLength: {
+                            value: 7,
+                            message: "Perm Number must be exactly 7 characters long."
+                        }
+                    })}
+                    placeholder="e.g. 0000000"
+                    defaultValue={initialContents?.perm_number}
+                />
+                <Form.Control.Feedback type="invalid">
+                    {errors.perm_number?.message}
+                </Form.Control.Feedback>
+            </Form.Group>
+
+            <Form.Group className="mb-3" >                          
+                <Form.Label htmlFor="description">Description</Form.Label>
+                <Form.Control
+                    data-testid={testIdPrefix + "-description"}
+                    id="description"
+                    type="text"
+                    isInvalid={Boolean(errors.description)}
+                    {...register("description", {
+                        required: "Description is required."
+                    })}
+                    placeholder="e.g. My legs are broken."  
+                    defaultValue={initialContents?.description}
+                />
+                <Form.Control.Feedback type="invalid">
+                    {errors.description?.message}
+                </Form.Control.Feedback>
+            </Form.Group>
+
+            <Button
+                type="submit"
+                data-testid={testIdPrefix + "-submit"}
+            >
+                {buttonLabel}
+            </Button>
+            <Button
+                variant="Secondary"
+                onClick={() => navigate(-1)}
+                data-testid={testIdPrefix + "-cancel"}
+            >
+                Cancel
+            </Button>
+
+        </Form>
+
+    )
+}
+
+export default RiderApplicationForm;
---
+++ frontend/src/tests/components/RiderApplication/RiderApplicationForm.test.js
@@ -0,0 +1,77 @@
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import RiderApplicationForm from "main/components/RiderApplication/RiderApplicationForm";
+import { riderApplicationFixtures } from "fixtures/riderApplicationFixtures";
+
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const mockedNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => mockedNavigate
+}));
+
+describe("RiderApplicationForm tests", () => {
+    const queryClient = new QueryClient();
+
+    const expectedHeaders = ["Email", "Perm Number", "Description"];
+    const testId = "RiderApplicationForm";
+
+    test("renders correctly with no initialContents", async () => {
+        render(
+            <QueryClientProvider client={queryClient}>
+                <Router>
+                    <RiderApplicationForm />
+                </Router>
+            </QueryClientProvider>
+        );
+
+        expect(await screen.findByText(/Create/)).toBeInTheDocument();
+
+        expectedHeaders.forEach((headerText) => {
+            const header = screen.getByText(headerText);
+            expect(header).toBeInTheDocument();
+          });
+
+    });
+
+    test("renders correctly when passing in initialContents", async () => {
+        render(
+            <QueryClientProvider client={queryClient}>
+                <Router>
+                    <RiderApplicationForm initialContents={riderApplicationFixtures.oneApplication} />
+                </Router>
+            </QueryClientProvider>
+        );
+
+        expect(await screen.findByText(/Create/)).toBeInTheDocument();
+
+        expectedHeaders.forEach((headerText) => {
+            const header = screen.getByText(headerText);
+            expect(header).toBeInTheDocument();
+        });
+
+        expect(await screen.findByTestId(`${testId}-id`)).toBeInTheDocument();
+        expect(screen.getByText(`Applicant Id`)).toBeInTheDocument();
+    });
+
+
+    test("that navigate(-1) is called when Cancel is clicked", async () => {
+        render(
+            <QueryClientProvider client={queryClient}>
+                <Router>
+                    <RiderApplicationForm />
+                </Router>
+            </QueryClientProvider>
+        );
+        expect(await screen.findByTestId(`${testId}-cancel`)).toBeInTheDocument();
+        const cancelButton = screen.getByTestId(`${testId}-cancel`);
+
+        fireEvent.click(cancelButton);
+
+        await waitFor(() => expect(mockedNavigate).toHaveBeenCalledWith(-1));
+    });
+
+});
