2cc3d357c74fdf8bd1cef11429e2cac356b5b99d
jiangtianwang <jiangtianwang@ucsb.edu>
2023-08-24 22:56:05 -0700

    renamed files

---
+++ frontend/src/main/pages/RiderApplication/RiderApplicationIndexPageMember.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useBackend } from 'main/utils/useBackend';
+
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import {useCurrentUser} from 'main/utils/currentUser'
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+import RiderApplicationTable from "main/components/RiderApplication/RiderApplicationTable";
+
+export default function RiderApplicationIndexPage() {
+
+    const currentUser = useCurrentUser();
+
+    const { data: riderApplications, error: _error, status: _status } =
+        useBackend(
+            // Stryker disable all : hard to test for query caching
+            ["/api/rider"],
+            { method: "GET", url: "/api/rider" },
+            []
+            // Stryker restore all 
+    );  
+    
+    return (
+          <BasicLayout>
+              <div className="pt-2">
+                <Button style={{ float: "right" }} as={Link} to="/apply/rider/new">
+                    New Rider Application
+                </Button>
+                <h1>Rider Applications</h1>
+                <RiderApplicationTable riderApplications={riderApplications} currentUser={currentUser} />
+              </div>
+          </BasicLayout>
+      );
+}
