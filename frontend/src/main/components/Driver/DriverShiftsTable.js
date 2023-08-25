4db51b34b11a3903172e98dab57c1b435c800154
Aditya Patil <77710426+adityapatil834@users.noreply.github.com>
2023-08-25 09:29:14 -0700

    Update frontend/src/main/components/Driver/DriverShiftsTable.js
    Co-authored-by: Chris Chang <51393127+chriscerie@users.noreply.github.com>

--- frontend/src/main/components/Driver/DriverShiftsTable.js
+++ frontend/src/main/components/Driver/DriverShiftsTable.js
@@ -1,31 +1,30 @@
 import OurTable from "main/components/OurTable"
 
 
+const columns = [
+    {
+        Header: 'id',
+        accessor: 'id', // accessor is the "key" in the data
+    },
+    {
+        Header: 'Day',
+        accessor: 'day',
+    },
+    {
+        Header: 'Shift start',
+        accessor: 'shiftStart',
+    },
+    {
+        Header: 'Shift end',
+        accessor: 'shiftEnd',
+    },
+    {
+        Header: 'Backup driver',
+        accessor: 'driverBackupID',
+    }
+];
+
 export default function DriverShiftsTable({ drivershifts }) {
-
-    const columns = [
-        {
-            Header: 'id',
-            accessor: 'id', // accessor is the "key" in the data
-        },
-        {
-            Header: 'Day',
-            accessor: 'day',
-        },
-        {
-            Header: 'Shift start',
-            accessor: 'shiftStart',
-        },
-        {
-            Header: 'Shift end',
-            accessor: 'shiftEnd',
-        },
-        {
-            Header: 'Backup driver',
-            accessor: 'driverBackupID',
-        }
-    ];
-
     return <OurTable
         data={drivershifts}
         columns={columns}
