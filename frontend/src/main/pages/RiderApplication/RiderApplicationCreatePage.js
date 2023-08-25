1d2ded151d1429c0dc8eac00b8af6bd83764243f
jiangtianwang <jiangtianwang@ucsb.edu>
2023-08-25 00:30:55 -0700

    Implemented RiderApplicationCreate Page

--- frontend/src/main/pages/RiderApplication/RiderApplicationCreatePage.js
+++ frontend/src/main/pages/RiderApplication/RiderApplicationCreatePage.js
@@ -1,13 +1,47 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import RiderApplicationForm from "main/components/RiderApplication/RiderApplicationForm";
+import { Navigate } from 'react-router-dom'
+import { useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
 
-export default function RiderApplicationCreatePage() {
+export default function RideRequestCreatePage() {
 
-  // Stryker disable all : placeholder for future implementation
+  const objectToAxiosParams = (riderApplication) => ({
+      url: "/api/riderApplication/new",
+      method: "POST",
+      params: {
+          perm_number: riderApplication.perm_number,
+          description: riderApplication.description,
+      }
+  });
+
+  const onSuccess = (riderApplication) => {
+      toast(`New Rider Application Made - id: ${riderApplication.id}`);
+  }
+
+  const mutation = useBackendMutation(
+      objectToAxiosParams,
+      { onSuccess },
+      // Stryker disable next-line all : hard to set up test for caching
+      ["/api/riderApplication/new"]
+  );
+
+  const { isSuccess } = mutation
+
+  const onSubmit = async (data) => {
+      mutation.mutate(data);
+  }
+
+  if (isSuccess) {
+      return <Navigate to="/apply/rider" />
+  }
+
   return (
-    <BasicLayout>
+      <BasicLayout>
       <div className="pt-2">
-        <h1>Create page not yet implemented</h1>
+          <h1>Submit New Rider Application</h1>
+          <RiderApplicationForm submitAction={onSubmit} />
       </div>
-    </BasicLayout>
+      </BasicLayout>
   )
 }
