5106b5c7ebe718cf1456db370f5357a59c242018
liamlizard <82777456+liamlizard@users.noreply.github.com>
2024-02-29 17:49:31 -0800

    Initial Commit

---
+++ src/main/java/edu/ucsb/cs156/gauchoride/controllers/DriverAvailabilityController.java
@@ -0,0 +1,65 @@
+package edu.ucsb.cs156.gauchoride.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
+
+@Tag(name = "DriverAvailability")
+@RequestMapping("/api/driverAvailability")
+@RestController
+@Slf4j
+public class DriverAvailabilityController extends ApiController{
+    
+    @Autowired
+    DriverAvailabilityRepository driverAvailabilityRepository;
+
+    //Allows driver to create and post new availability
+    @Operation(summary= "Create a new Driver Availability")
+    @PreAuthorize("hasRole('ROLE_DRIVER')")
+    @PostMapping("/new")
+    public DriverAvailability postDriverAvailability(
+            @Parameter(name="driverId") @RequestParam long driverId,
+            @Parameter(name="day") @RequestParam String day,
+            @Parameter(name="startTime") @RequestParam int startTime,
+            @Parameter(name="endTime") @RequestParam String endTime,
+            @Parameter(name="notes") @RequestParam String notes)
+            throws JsonProcessingException {
+
+        // For an explanation of @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+        // See: https://www.baeldung.com/spring-date-parameters
+
+        log.info("notes={}", notes);
+
+        DriverAvailability driverAvailability = new DriverAvailability();
+        driverAvailability.setDriverId(driverId);
+        driverAvailability.setDay(day);
+        driverAvailability.setStartTime(startTime);
+        driverAvailability.setEndTime(endTime);
+        driverAvailability.setNotes(notes);
+
+        DriverAvailability savedDriverAvailability = driverAvailabilityRepository.save(driverAvailability);
+
+        return savedDriverAvailability;
+    }
+
+
+    //Lets admins get all driver availabilties
+    @Operation(summary= "List all Driver Availabilities")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/admin/all")
+    public Iterable<DriverAvailability> allDriverAvailabilities() {
+        Iterable<DriverAvailability> availabilities = driverAvailabilityRepository.findAll();
+        return availabilities;
+    }
+
+}
