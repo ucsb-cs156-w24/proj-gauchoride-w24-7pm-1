ddabfed99409ce1e710509ea7ac984e58f553fea
Zeel <zpatel@ucsb.edu>
2024-03-05 17:54:08 -0800

    added all the admin pages including the index and review pages

---
+++ frontend/src/main/pages/RiderApplication/RiderApplicationEditPageAdmin.js
@@ -0,0 +1,68 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import { useParams } from "react-router-dom";
+import RiderApplicationEditForm from "main/components/RiderApplication/RiderApplicationEditForm";
+import { Navigate } from 'react-router-dom'
+import { useBackend, useBackendMutation } from "main/utils/useBackend";
+
+import { toast } from "react-toastify";
+
+export default function RiderApplicationEditPage() {
+  let { id } = useParams();
+
+  const { data: riderApplication, _error, _status } =
+    useBackend(
+      // Stryker disable next-line all : don't test internal caching of React Query
+      [`/api/riderApplication?id=${id}`],
+      {  // Stryker disable next-line all : GET is the default, so changing this to "" doesn't introduce a bug
+        method: "GET",
+        url: `/api/riderApplication`,
+        params: {
+          id
+        }
+      }
+    );
+
+
+  const objectToAxiosPutParams = (riderApplication) => ({
+    url: "/api/riderApplication",
+    method: "PUT",
+    params: {
+      id: riderApplication.id,
+    },
+    data: {
+        notes: riderApplication.notes
+    }
+  });
+
+  const onSuccess = (riderApplication) => {
+    toast(`Application Updated - id: ${riderApplication.id}`);
+  }
+
+  const mutation = useBackendMutation(
+    objectToAxiosPutParams,
+    { onSuccess },
+    // Stryker disable next-line all : hard to set up test for caching
+    [`/api/riderApplication?id=${id}`]
+  );
+
+  const { isSuccess } = mutation
+
+  const onSubmit = async (data) => {
+    mutation.mutate(data);
+  }
+
+  if (isSuccess) {
+    return <Navigate to="/apply/rider" />
+  }
+
+    return (
+        <BasicLayout>
+            <div className="pt-2">
+                <h1>Edit Rider Application</h1>
+                {riderApplication &&
+                <RiderApplicationEditForm initialContents={riderApplication} submitAction={onSubmit} buttonLabel="Edit" />
+                }
+            </div>
+        </BasicLayout>
+    )
+}
