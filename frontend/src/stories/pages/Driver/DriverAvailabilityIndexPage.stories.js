27d2b9600a46256f0056041a7656fd7adda3158f
James Pflaging <pflagingjames@gmail.com>
2024-03-07 15:06:38 -0800

    create, edit, index stories

---
+++ frontend/src/stories/pages/Driver/DriverAvailabilityIndexPage.stories.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+import DriverAvailabilityIndexPage from 'main/pages/Ride/DriverAvailabilityIndexPage';
+
+export default {
+    title: 'pages/Driver/DriverAvailabilityIndexPage',
+    component: DriverAvailabilityIndexPage
+};
+
+const Template = () => <DriverAvailabilityIndexPage />;
+
+export const Default = Template.bind({});
