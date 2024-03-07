f945469d700ba8a69f467337fbd4dc01961d7192
James Pflaging <pflagingjames@gmail.com>
2024-03-07 15:06:38 -0800

    create, edit, index stories

---
+++ frontend/src/stories/pages/Driver/DriverAvailabilityCreatePage.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import RideRequestCreatePage from 'main/pages/Ride/DriverAvailabilityCreatePage';
+
+export default {
+    title: 'pages/Driver/DriverAvailabilityCreatePage',
+    component: RideRequestCreatePage
+};
+
+const Template = () => <DriverAvailabilityCreatePage />;
+
+export const Default = Template.bind({});
