42042956154e65a70386d9a9a0c9a551f875c410
Zeel <zpatel@ucsb.edu>
2024-03-07 18:25:38 -0800

    Revert "adding Stryker disable and restore"
    This reverts commit 5682ae3db03f8456a0a13d72cd284a0cd2c49bf5.

--- frontend/src/main/components/Driver/DriverAvailabilityTable.js
+++ frontend/src/main/components/Driver/DriverAvailabilityTable.js
@@ -59,7 +59,6 @@ export default function DriverAvailabilityTable({
         }
     ];
 
-    // Stryker disable all : hard to test for query caching
     const buttonColumnsDriver = [
         ...columns,
         ButtonColumn("Edit", "primary", editCallback, "DriverAvailabilityTable"),
@@ -70,7 +69,7 @@ export default function DriverAvailabilityTable({
         ...columns,
         ButtonColumn("Review", "primary", reviewCallback, "DriverAvailabilityTable")
     ];
-    // Stryker restore all
+
     const columnsToDisplay = (hasRole(currentUser, "ROLE_ADMIN")) ? buttonColumnsAdmin : (hasRole(currentUser, "ROLE_DRIVER")) ? buttonColumnsDriver : columns;
 
     return <OurTable
